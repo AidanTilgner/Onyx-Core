6f2177d74205bc041503b2471469a36118233db2
Aidan Tilgner <aidantilgner02@gmail.com>
2022-11-22 13:39:55 -0800

    added some ability to use middleware in other modules and also pm2 conf

---
+++ pm2.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [
+    {
+      name: "Onyx-Core",
+      script: "npm run start",
+    },
+  ],
+};
--- src/people/interface.ts
+++ src/people/interface.ts
@@ -2,6 +2,11 @@ import { Interface } from "docs/interfaces";
 import { initDefaultUser } from "./module/utils/auth";
 import { initIO } from "./module/utils/socket-io";
 import { initDB } from "./module/utils/db";
+import {
+  authenticateToken,
+  authenticateSuperUser,
+  authenticateHyperUser,
+} from "./module/middleware/auth";
 
 class PeopleInterface extends Interface {
   public initDefaultUser = initDefaultUser;
@@ -10,6 +15,14 @@ class PeopleInterface extends Interface {
 
   constructor() {
     super("People");
+  }
+
+  public useMiddleware() {
+    return {
+      authenticateToken,
+      authenticateSuperUser,
+      authenticateHyperUser,
+    };
   }
 }
 
